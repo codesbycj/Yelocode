15b88ed9e765cdaaa5df62a5783f1086e668ce36
Developergatsby <gabokekeemmanuel@gmail.com>
2025-06-12 07:55:14 +0100

    About Us

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function startCounting() {
       const theCount = setInterval(() => {
         if (start < end) {
           start++;
-          e.textContent = `${start}+`;
+          e.textContent = `${start}`;
         } else {
           clearInterval(theCount);
         }
@@ -26,3 +26,17 @@ function startCounting() {
 }
 
 window.addEventListener("scroll", startCounting);
+
+
+let pov = document.querySelector('.footerHead');
+let ftHead1 = document.querySelector('.footerHead h1');
+
+pov.addEventListener('mousemove', e => {
+  const rect = pov.getBoundingClientRect();
+  const x = e.clientX - rect.left;
+  const y = e.clientY - rect.top;
+  ftHead1.style.opacity = 1;
+
+  pov.style.setProperty('--x', x + 'px');
+  pov.style.setProperty('--y', y + 'px');
+})
